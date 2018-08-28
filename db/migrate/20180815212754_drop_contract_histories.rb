4edbfa8bc245b955789180e06b65564ec1810495
Joon Lee <joon.lee@ucla.edu>
2018-08-27 22:14:56 -0700

    fix DropContractHistories migration to convert contract history records to contract records

--- db/migrate/20180815212754_drop_contract_histories.rb
+++ db/migrate/20180815212754_drop_contract_histories.rb
@@ -1,29 +1,40 @@
 class DropContractHistories < ActiveRecord::Migration[5.2]
   def change
     # Manually query contract_histories and create matching Contracts
-    Contract.transaction do
-      config = Rails.configuration.database_configuration[Rails.env]
+    config = Rails.configuration.database_configuration[Rails.env]
 
-      client = PG::Connection.open(
-        host: config['host'],
-        dbname: config['database'],
-        user: config['username'],
-        password: config['password'],
+    client = PG::Connection.open(
+      host: config['host'],
+      dbname: config['database'],
+      user: config['username'],
+      password: config['password'],
+    )
+
+    histories = client.query 'SELECT * FROM contract_histories'
+    contract_player_ids = Contract.pluck(:id, :player_id).to_h
+    new_contracts = []
+
+    histories.each do |ch|
+      next if Contract.exists?(
+        player_id: contract_player_ids[ch['contract_id'].to_i],
+        signed_date: ch['datestamp']
       )
 
-      histories = client.query 'SELECT * FROM contract_histories'
-      contract_player_ids = Contract.pluck(:id, :player_id).to_h
-      new_contracts = []
-
-      histories.each do |history|
-
-        new_contracts << {
-          player_id: 
-        }
-      end
-
-      Contract.import new_contracts, validate: false
+      new_contracts << {
+        player_id:         contract_player_ids[ch['contract_id'].to_i],
+        signed_date:       ch['datestamp'],
+        wage:              ch['wage'],
+        signing_bonus:     ch['signing_bonus'],
+        release_clause:    ch['release_clause'],
+        performance_bonus: ch['performance_bonus'],
+        bonus_req:         ch['bonus_req'],
+        bonus_req_type:    ch['bonus_req_type'],
+        effective_date:    ch['effective_date'],
+        end_date:          ch['end_date'],
+      }
     end
+
+    Contract.import new_contracts, validate: false
 
     drop_table :contract_histories
   end
