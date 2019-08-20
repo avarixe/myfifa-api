dbd6b7882c13746d865d7d03c6a14cdc69112526
Joon Lee <joon.lee@ucla.edu>
2019-08-20 15:09:42 -0700

    update config for Rails 6.0

--- app/models/fixture.rb
+++ app/models/fixture.rb
@@ -8,8 +8,6 @@
 #  stage_id   :bigint(8)
 #  home_team  :string
 #  away_team  :string
-#  home_score :string
-#  away_score :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
