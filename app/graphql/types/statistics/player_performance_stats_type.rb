5887f4b2df48f7c787994566dc249b0da8a2c6e4
Joon Lee <l33j00n325@gmail.com>
2021-07-21 10:16:50 -0700

    add graphql statistics types descriptions

--- app/graphql/types/statistics/player_performance_stats_type.rb
+++ app/graphql/types/statistics/player_performance_stats_type.rb
@@ -3,14 +3,39 @@
 module Types
   module Statistics
     class PlayerPerformanceStatsType < BaseObject
-      field :player_id, ID, null: false
-      field :season, Integer, null: false
-      field :competition, String, null: false
-      field :num_matches, Integer, null: false
-      field :num_minutes, Integer, null: false
-      field :num_goals, Integer, null: false
-      field :num_assists, Integer, null: false
-      field :num_clean_sheets, Integer, null: false
+      description 'Player Match Statistics in a Competition and Season'
+
+      stats_field_suffix = 'by Player this Season and Competition'
+
+      field :player_id, ID, 'ID of Player', null: false
+      field :season,
+            Integer,
+            'Number of complete years after Team Start and this Season',
+            null: false
+      field :competition,
+            String,
+            'Name of Competition',
+            null: false
+      field :num_matches,
+            Integer,
+            "Number of Matches capped #{stats_field_suffix}",
+            null: false
+      field :num_minutes,
+            Integer,
+            "Number of Minutes played #{stats_field_suffix}",
+            null: false
+      field :num_goals,
+            Integer,
+            "Number of Goals scored #{stats_field_suffix}",
+            null: false
+      field :num_assists,
+            Integer,
+            "Number of Goals assisted #{stats_field_suffix}",
+            null: false
+      field :num_clean_sheets,
+            Integer,
+            "Number of Matches with Clean Sheets capped #{stats_field_suffix}",
+            null: false
     end
   end
 end
--- app/graphql/types/statistics/transfer_activity_type.rb
+++ app/graphql/types/statistics/transfer_activity_type.rb
@@ -3,10 +3,24 @@
 module Types
   module Statistics
     class TransferActivityType < BaseObject
-      field :arrivals, [Myfifa::ContractType], null: false
-      field :departures, [Myfifa::ContractType], null: false
-      field :transfers, [Myfifa::TransferType], null: false
-      field :loans, [Myfifa::LoanType], null: false
+      description 'Collection of Player Arrivals and Departures'
+
+      field :arrivals,
+            [Myfifa::ContractType],
+            'List of new Contracts (i.e. excluding Renewals)',
+            null: false
+      field :departures,
+            [Myfifa::ContractType],
+            'List of Contract expirations excluding Transfers',
+            null: false
+      field :transfers,
+            [Myfifa::TransferType],
+            'List of Transfers',
+            null: false
+      field :loans,
+            [Myfifa::LoanType],
+            'List of Loans',
+            null: false
     end
   end
 end
